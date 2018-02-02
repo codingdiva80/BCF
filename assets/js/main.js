273fa7933e248858b67b7c8b44cb682357b5c701
Luna <altynaylinstrum@yahoo.com>
2018-02-02 11:07:30 -0800

    Testimonials pages completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,12 @@ function respNav() {
         x.className = "navbar";
     }
 }
+function displayTestimonials() {
+    let divToDisplay = document.getElementById("testimonials-container-2");
+    let buttonDisplay = document.getElementById("displayButton");
+    buttonDisplay.addEventListener("click", function() {
+        divToDisplay.style.display = "flex";
+        buttonDisplay.style.display = "none";
+    })
+}
+// displayTestimonials();
