d920160b0b1e8e72d6a9f49cc5ae87fe054dbc3e
Luna <altynaylinstrum@yahoo.com>
2018-02-02 21:26:36 -0800

    alert

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
-let touchEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
-
 function displayTestimonials() {
+    alert("workig");
     let divToDisplay = document.getElementById("testimonials-container-2");
     let buttonDisplay = document.getElementById("displayButton");
         divToDisplay.style.display = "flex";
