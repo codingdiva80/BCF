5fc5290a289278804553763e4c1dfa3882500f58
Luna <altynaylinstrum@yahoo.com>
2018-02-02 21:19:23 -0800

    onclick changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,6 @@ let touchEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
 function displayTestimonials() {
     let divToDisplay = document.getElementById("testimonials-container-2");
     let buttonDisplay = document.getElementById("displayButton");
-    buttonDisplay.addEventListener("click", function() {
         divToDisplay.style.display = "flex";
         buttonDisplay.style.display = "none";
-    });
-    buttonDisplay.addEventListener(touchEvent, function() {
-        divToDisplay.style.display = "flex";
-        buttonDisplay.style.display = "none";
-    });
 }
