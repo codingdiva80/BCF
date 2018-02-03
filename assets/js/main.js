0a67f2bec4d6a4f959a2a1abcda960295d527343
Luna <altynaylinstrum@yahoo.com>
2018-02-02 21:15:03 -0800

    touch event added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,14 @@
-function respNav() {
-    let x = document.getElementById("resNav");
-    if (x.className === "navbar") {
-        x.className += " responsive";
-    } else {
-        x.className = "navbar";
-    }
-}
+let touchEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
+
 function displayTestimonials() {
     let divToDisplay = document.getElementById("testimonials-container-2");
     let buttonDisplay = document.getElementById("displayButton");
     buttonDisplay.addEventListener("click", function() {
         divToDisplay.style.display = "flex";
         buttonDisplay.style.display = "none";
-    })
+    });
+    buttonDisplay.addEventListener(touchEvent, function() {
+        divToDisplay.style.display = "flex";
+        buttonDisplay.style.display = "none";
+    });
 }
-// displayTestimonials();
