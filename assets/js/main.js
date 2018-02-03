8d4388989eac9be3cf4b8b51c81a00534d041211
Luna <altynaylinstrum@yahoo.com>
2018-02-02 22:06:27 -0800

    eventlistener4

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,10 @@ let touchEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
 
 function displayTestimonials() {
     let divToDisplay = document.getElementById("testimonials-container-2");
-    let buttonDisplay = document.getElementById("displayButton");
+    let buttonDisplay = document.getElementById("displayTestimonialsTag");
     buttonDisplay.addEventListener(touchEvent, ()=>{
         divToDisplay.style.display = "flex";
         buttonDisplay.style.display = "none";
     });
        
 }
-
-window.addEventListener(touchEvent, function(){
-    alert("Welcome");
-});
