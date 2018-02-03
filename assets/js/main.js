cce37f89be86bc4bb3aa0a703436f735ea392ff7
Luna <altynaylinstrum@yahoo.com>
2018-02-02 21:45:43 -0800

    eventlistener for windows

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ let touchEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
 function displayTestimonials() {
     let divToDisplay = document.getElementById("testimonials-container-2");
     let buttonDisplay = document.getElementById("displayButton");
-    document.addEventListener(touchEvent, ()=>{
+    buttonDisplay.addEventListener(touchEvent, ()=>{
         alert("Iphone clicked");
         divToDisplay.style.display = "flex";
         buttonDisplay.style.display = "none";
