b99211af0b9b1882dc1d061640ec1046b1cc758f
joe44850 <jlinstrum@dropbox.com>
2018-02-04 13:14:43 -0800

    Adjusted menu show hide

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,10 @@
 let touchEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
 
+function init(){
+    displayTestimonials();        
+    showHideMenu();
+}
+
 function displayTestimonials() {
     let divToDisplay = document.getElementById("testimonials-container-2");
     let buttonDisplay = document.getElementById("displayTestimonialsTag");
@@ -7,5 +12,24 @@ function displayTestimonials() {
         divToDisplay.style.display = "flex";
         buttonDisplay.style.display = "none";
     });
-       
+}
+
+function showHideMenu(){
+    toggleMenu();
+    let hamburgerMenu = document.querySelector("#toggle-label");
+    let navMenu = document.querySelector("#navMenu");
+    navMenu.addEventListener(touchEvent, (e)=>{
+        document.location.href = e.target.href;
+        toggleMenu();
+    });
+    hamburgerMenu.addEventListener(touchEvent, (e)=>{
+        toggleMenu();
+    });
+}
+
+function toggleMenu(){
+    let navMenu = document.querySelector("#navMenu");
+    let d = navMenu.getAttribute("class");
+    navMenu.setAttribute("class", (d === "navMenuHide") ? "navMenuShow" : "navMenuHide");
+    console.log(navMenu.getAttribute("class"));
 }
